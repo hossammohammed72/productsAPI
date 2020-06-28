263fd3e3fa61d46c0e739abcd3e931eddbf29248
Hossam <hossammohammed72@gmail.com>
2020-06-28 12:26:48 +0200

    Added Product Attribute Utils

---
+++ app/Utils/ProductAttributesUtil.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace App\Utils;
+
+
+class ProductAttributesUtil
+{
+    CONST ID='id';
+    CONST NAME='name';
+    CONST STATUS='status';
+    CONST VAT='vat';
+    CONST CURRENTPRICE ='currentPrice';
+    CONST ORIGINALPRICE ='originalPrice';
+    CONST CURRENCY ='currency';
+    CONST ENDDATE = 'endDate';
+    CONST DESCRIPTION = 'description';
+
+}
